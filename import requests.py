b7c1099b88dcb9677883e945478faa492c4168d7
ctshiz <76063863+ctshiz@users.noreply.github.com>
2021-03-19 02:01:15 -0300

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -1,5 +1,7 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url, json={'Type': 1, 'Torque [Nm]': 4.5})
+r = requests.post(url, json={'Torque [Nm]': 1.924952, 'Tool wear [minNm': 0.915434})
+r_hdf = requests.post_hdf(url, json={'Type': 1, 'Dif temperature [K]': -8.4, 'Rotational speed [rpm]': 1363})
 print(r.json())
+print(r_hdf.json())
